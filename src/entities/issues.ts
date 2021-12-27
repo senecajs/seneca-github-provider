8ac69396048ea55f94fed6dea5b101f6e9efee54
Gustavo <guhmerces@hotmail.com>
2021-12-27 18:21:42 -0300

    Create load_issue() body

--- src/entities/issues.ts
+++ src/entities/issues.ts
@@ -1,6 +1,22 @@
 function issues(args: any) {
   async function load_issue(this: any, msg: any) {
+    const repo_id = msg.q.id
+    const issue_number = msg.q.issue_number
 
+    const [ownername, reponame]: [string, string] = repo_id.split("/")
+
+    const res = await args.octokit.rest.issues.get({
+      owner: ownername,
+      repo: reponame,
+      issue_number,
+    })
+
+    const issue: any = res.data
+
+    issue.repo_id = repo_id
+    issue.issue_number = issue_number
+
+    return this.make$(args.ZONE_BASE + "issue").data$(issue)
   }
 
   async function save_issue(this: any, msg: any) {
