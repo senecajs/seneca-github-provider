3208b77e3b5c689d28f3e603698565cee9ae52ac
Gustavo <guhmerces@hotmail.com>
2021-12-27 17:13:00 -0300

    create issues closure
    wraps actions related to "Issues" from Github API

---
+++ src/entities/issues.ts
@@ -0,0 +1,16 @@
+function issues(args: any) {
+  async function load_issue(this: any, msg: any) {
+
+  }
+
+  async function save_issue(this: any, msg: any) {
+
+  }
+
+  return {
+    load_issue,
+    save_issue,
+  }
+}
+
+export default issues
