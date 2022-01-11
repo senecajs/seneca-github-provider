13a78f671566f5afcb74584088dc9dfdbf2ee1ad
Gustavo <guhmerces@hotmail.com>
2022-01-11 07:11:20 -0300

    Add test attribute to CommandDetails type

--- src/types.ts
+++ src/types.ts
@@ -7,29 +7,50 @@ type CommandFn = (
   include?: string[]
 ) => (msg: any) => Promise<any>
 
-type Action = (arg: any) => Promise<any>
+type CommandTest = {
+  args: Record<string, any>
+  expectations?: Record<string, Assertions>
+}
+
+type Assertions = {
+  sameAs?: any,
+  toBe?: TestToBe[]
+}
+
+type TestToBe = "defined" | "falsy"
 
 type CommandDetails = {
   cmd: string
   action: string
   rest_endpoint: GithubRestEndpoints
   body_args?: string[]
+  test?: CommandTest
   include?: string[]
 }
 
 type EntityMap = {
   [name: string] : {
-    active: Boolean
+    fields: string[]
     commands: CommandDetails[]
   }
 }
 
-type GithubRestEndpoints = "repos" | "branches" | "checks" | "codesOfConduct" | "codeScanning" | "commit" | "gists" | "issues" | "licenses" | "orgs" | "projects" | "pulls" | "releases" | "secretScanning" | "teams" | "users"
+type GithubRestEndpoints =
+  | "repos"
+  | "repos.branches"
+  | "checks"
+  | "codesOfConduct"
+  | "codeScanning"
+  | "git"
+  | "gists"
+  | "issues"
+  | "licenses"
+  | "orgs"
+  | "projects"
+  | "pulls"
+  | "releases"
+  | "secretScanning"
+  | "teams"
+  | "users"
 
-export type {
-    Command,
-    CommandFn,
-    CommandDetails,
-    Action,
-    EntityMap,
-}
+export type { Command, CommandFn, CommandDetails, EntityMap }
