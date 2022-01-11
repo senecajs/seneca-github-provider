afbab16df1e4f8b6f0a10c207d76a79f63983906
Gustavo <guhmerces@hotmail.com>
2022-01-11 07:08:52 -0300

    Fix arguments passed to cmd_handlers()

--- src/identify-handler.ts
+++ src/identify-handler.ts
@@ -4,7 +4,7 @@ import { CommandDetails } from "./types";
 function identify_handler(command_details: CommandDetails, github_action: CallableFunction) {
   const cmd_name = command_details.cmd
 
-  const handlers: any = cmd_handlers(github_action, { ...command_details })
+  const handlers: any = cmd_handlers(github_action, { ...command_details.body_args }, command_details.include)
 
   const handler = handlers[cmd_name]
 
