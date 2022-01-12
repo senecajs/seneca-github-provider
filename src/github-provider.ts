9481b64516995fa81d2314b48aea81172cebd46c
Gustavo <guhmerces@hotmail.com>
2022-01-12 17:57:25 -0300

    Remove unused save_repo and load_repo methods

--- src/github-provider.ts
+++ src/github-provider.ts
@@ -69,53 +69,6 @@ function GithubProvider(this: any, _options: any) {
     }
   }
 
-  async function load_repo(this: any, msg: any) {
-    let ent: any = null
-
-    let q: any = msg.q
-    let [ownername, reponame]: [string, string] = q.id.split('/')
-
-    let res = await octokit.rest.repos.get({
-      owner: ownername,
-      repo: reponame,
-    })
-
-    if (res && 200 === res.status) {
-      let data: any = res.data
-      data.github_id = data.id
-      data.id = q.id
-      ent = this.make$(ZONE_BASE + 'repo').data$(data)
-    }
-
-    return ent
-  }
-
-
-  async function save_repo(this: any, msg: any) {
-    let ent: any = msg.ent
-
-    let [ownername, reponame]: [string, string] = ent.id.split('/')
-
-    let data = {
-      owner: ownername,
-      repo: reponame,
-      description: ent.description
-    }
-
-    let res = await octokit.rest.repos.update(data)
-
-    if (res && 200 === res.status) {
-      let data: any = res.data
-      data.github_id = data.id
-      data.id = ownername + '/' + reponame
-      ent = this.make$(ZONE_BASE + 'repo').data$(data)
-    }
-
-    return ent
-  }
-
-
-
   seneca.prepare(async function(this: any) {
     let out = await this.post('sys:provider,get:key,provider:github,key:api')
     if (!out.ok) {
