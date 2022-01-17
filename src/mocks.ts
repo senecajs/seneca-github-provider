3b820d48c38ee07888c45b286d60f689d627fc11
Gustavo <guhmerces@hotmail.com>
2022-01-17 08:23:59 -0300

    Add mock data to mocks.ts file

--- src/mocks.ts
+++ src/mocks.ts
@@ -1,14 +1,136 @@
 export default {
+  repo: {
+    patch: {
+      method: "PATCH",
+      url: "/repos/:owner/:repo",
+      mock_data: {
+        repo_id: "senecajs/seneca",
+      },
+    },
+    get: {
+      method: "GET",
+      url: "/repos/:owner/:repo",
+      mock_data: {
+        repo_id: "senecajs/seneca",
+      },
+    },
+  },
+  code_scanning: {
+    patch: {
+      method: "PATCH",
+      url: "/repos/:owner/:repo/code-scanning/alerts/:alert_number",
+      mock_data: {
+        dismissed_reason: "used in tests",
+      },
+    },
+    get: {
+      method: "GET",
+      url: "/repos/:owner/:repo/code-scanning/alerts/:alert_number",
+      mock_data: {
+        number: 1,
+      },
+    },
+  },
+  gist: {
+    patch: {
+      method: "PATCH",
+      url: "/gists/:gist_id",
+      mock_data: {
+        id: "some-gist-id",
+      },
+    },
+    get: {
+      method: "GET",
+      url: "/gists/:gist_id",
+      mock_data: {
+        id: "some-gist-id",
+      },
+    },
+  },
+  issue: {
+    patch: {
+      method: "PATCH",
+      url: "/repos/:owner/:repo/issues/:issue_number",
+      mock_data: {
+        repo_id: "senecajs/seneca",
+      },
+    },
+    get: {
+      method: "GET",
+      url: "/repos/:owner/:repo/issues/:issue_number",
+      mock_data: {
+        repo_id: "senecajs/seneca",
+        number: 1,
+      },
+    },
+  },
+  pull: {
+    patch: {
+      method: "PATCH",
+      url: "/repos/:owner/:repo/pulls/:pull_number",
+      mock_data: {
+        repo_id: "senecajs/seneca",
+      },
+    },
+    get: {
+      method: "GET",
+      url: "/repos/:owner/:repo/pulls/:pull_number",
+      mock_data: {
+        id: 819532044,
+      },
+    },
+  },
+  release: {
+    patch: {
+      method: "PATCH",
+      url: "/repos/:owner/:repo",
+      mock_data: {
+        repo_id: "senecajs/seneca",
+      },
+    },
+    get: {
+      method: "GET",
+      url: "/repos/:owner/:repo/releases/:release_id",
+      mock_data: {
+        id: 123456,
+      },
+    },
+  },
+  branch: {
+    get: {
+      method: "GET",
+      url: "/repos/:owner/:repo/branches/:branch",
+      mock_data: {
+        repo_id: "senecajs/seneca",
+        name: "master",
+      },
+    },
+  },
   code_of_conduct: {
     get: {
       method: "GET",
       url: "/codes_of_conduct/:key",
       mock_data: {
         key: "contributor_covenant",
-        name: "Contributor Covenant",
-        html_url: null,
-        url: "https://api.github.com/codes_of_conduct/contributor_covenant",
-        body: "# Contributor Covenant Code of Conduct\n\n## Our Pledge\n\nWe ... interrupted - mock data",
+      },
+    },
+  },
+  commit: {
+    get: {
+      method: "GET",
+      url: "/repos/:owner/:repo/git/commits/:commit_sha",
+      mock_data: {
+        repo_id: "senecajs/seneca",
+        sha: "commit-sha",
+      },
+    },
+  },
+  license: {
+    get: {
+      method: "GET",
+      url: "/licenses/:license",
+      mock_data: {
+        key: "mit",
       },
     },
   },
@@ -17,55 +139,68 @@ export default {
       method: "GET",
       url: "/orgs/:org",
       mock_data: {
-        login: "DemoOrganization20",
-        id: 96800940,
-        node_id: "O_kgDOBcUQrA",
-        url: "https://api.github.com/orgs/DemoOrganization20",
-        repos_url: "https://api.github.com/orgs/DemoOrganization20/repos",
-        events_url: "https://api.github.com/orgs/DemoOrganization20/events",
-        hooks_url: "https://api.github.com/orgs/DemoOrganization20/hooks",
-        issues_url: "https://api.github.com/orgs/DemoOrganization20/issues",
-        members_url:
-          "https://api.github.com/orgs/DemoOrganization20/members{/member}",
-        public_members_url:
-          "https://api.github.com/orgs/DemoOrganization20/public_members{/member}",
-        avatar_url: "https://avatars.githubusercontent.com/u/96800940?v=4",
-        description: null,
-        is_verified: false,
-        has_organization_projects: true,
-        has_repository_projects: true,
-        public_repos: 0,
-        public_gists: 0,
-        followers: 0,
-        following: 0,
-        html_url: "https://github.com/DemoOrganization20",
-        created_at: "2021-12-28T23:39:49Z",
-        updated_at: "2021-12-28T23:39:49Z",
-        type: "Organization",
-        total_private_repos: 0,
-        owned_private_repos: 0,
-        private_gists: 0,
-        disk_usage: 0,
-        collaborators: 0,
-        billing_email: "guhmerces@hotmail.com",
-        default_repository_permission: "read",
-        members_can_create_repositories: true,
-        two_factor_requirement_enabled: false,
-        members_allowed_repository_creation_type: "all",
-        members_can_create_public_repositories: true,
-        members_can_create_private_repositories: true,
-        members_can_create_internal_repositories: false,
-        members_can_create_pages: true,
-        members_can_fork_private_repositories: false,
-        members_can_create_public_pages: true,
-        members_can_create_private_pages: true,
-        plan: {
-          name: "free",
-          space: 976562499,
-          private_repos: 10000,
-          filled_seats: 1,
-          seats: 0,
-        },
+        login: "Organization123",
+      },
+    },
+  },
+  user: {
+    get: {
+      method: "GET",
+      url: "/users/:username",
+      mock_data: {
+        id: 45415308,
+      },
+    },
+  },
+  check: {
+    get: {
+      method: "GET",
+      url: "/repos/:owner/:repo/check-runs/:check_run_id",
+      mock_data: {
+        id: 1,
+      },
+    },
+    patch: {
+      method: "PATCH",
+      url: "/repos/:owner/:repo/check-runs/:check_run_id",
+      mock_data: {
+        id: 1,
+      },
+    },
+  },
+  project: {
+    get: {
+      method: "GET",
+      url: "/projects/:project_id",
+      mock_data: {
+        id: 10,
+      },
+    },
+    patch: {
+      method: "PATCH",
+      url: "/projects/:project_id",
+      mock_data: {
+        id: 10,
+      },
+    },
+  },
+  secret_scanning: {
+    get: {
+      method: "GET",
+      url: "/repos/:owner/:repo/secret-scanning/alerts/:alert_number",
+      mock_data: {
+        number: 11,
+        state: 'resolved',
+        resolution: 'used in tests',
+      },
+    },
+    patch: {
+      method: "PATCH",
+      url: "/repos/:owner/:repo/secret-scanning/alerts/:alert_number",
+      mock_data: {
+        number: 11,
+        state: 'resolved',
+        resolution: 'used in tests',
       },
     },
   },
