ca897fcc5b625ff29f43c5a7950d5cad129fa748
Gustavo <guhmerces@hotmail.com>
2022-01-21 07:05:28 -0300

    Refactor cmd-handlers.ts

--- src/cmd-handlers.ts
+++ src/cmd-handlers.ts
@@ -1,41 +1,16 @@
-function cmd_handlers(reqFn: CallableFunction, body_args: Array<string> = [], include?: string[]) {
-  async function handler(this:any, msg: any) {
-    let body: any = {}
-    let source: any
-    let args
-
-    switch (msg.cmd) {
-      case 'load':
-        source = {...msg.q}
-        args = source
-        break;
-      case 'save':
-        source = {...msg.ent}
-        args = {...msg.q}
-        break;
-    
-      default:
-        throw new Error('Unknown command : ' + msg.cmd)
-    }
-
-    if(source.repo_id) {
-      const [owner, repo] = source.repo_id.split('/')
-      body = {
-        owner,
-        repo,
-      }
-    }
+function make_actions(reqFn: CallableFunction, body_args: Array<string> = [], include?: string[]) {
+  async function load(this:any, msg: any) {
+    const source = {...msg.q}
+    let body: Record<string,any> = {}
 
     const old_source = {...source}
-    delete source.repo_id
 
-    body_args.forEach(body_arg => {
-      body[body_arg] = source[body_arg] 
-    })
-    
-    body = {...body, ...args}
+    if(source.repo_id) {
+      body = owner_repo(source.repo_id)
+      delete source.repo_id
+    }
 
-    let res = await reqFn(body)
+    let res = await reqFn({...body, ...source})
     res = res.data
 
     if (include) {
@@ -54,7 +29,59 @@ function cmd_handlers(reqFn: CallableFunction, body_args: Array<string> = [], in
     return this.make$(msg.ent.entity$).data$(res)
   }
 
-  return handler
+  async function save(this:any, msg: any) {
+    const source = {...msg.ent}
+    const args = {...msg.q}
+    let body: Record<string,any> = {}
+
+    const old_source = {...source}
+
+    if(source.repo_id) {
+      body = owner_repo(source.repo_id)
+      delete source.repo_id
+    }
+
+    body = fill_obj(source, body, body_args)
+
+    let res = await reqFn({...body, ...args})
+    res = res.data
+
+    if (include) {
+      include.forEach((item) => {
+        if (item.indexOf(' as ') !== -1) {
+          const [attr, new_attr_name] = item
+            .split(' as ')
+            .map((item) => item.trim())
+          res[new_attr_name] = old_source[attr]
+        } else {
+          res[item] = old_source[item]
+        }
+      })
+    }
+
+    return this.make$(msg.ent.entity$).data$(res)
+  }
+
+  function owner_repo(repo_id: string): Record<string, string> {
+    const [owner, repo] = repo_id.split('/')
+    return {
+      owner,
+      repo,
+    }
+  }
+
+  function fill_obj(from: Record<string, any>, to: Record<string, any>, attributes: Array<string>) {
+    attributes.forEach(attr => {
+      to[attr] = from[attr] 
+    })
+
+    return to
+  }
+
+  return {
+    load,
+    save
+  }
 }
 
-export default cmd_handlers
+export { make_actions }
