a3573901f5880102f2d2b0070204756806593c8c
Gustavo <guhmerces@hotmail.com>
2022-02-01 00:51:36 -0300

    Add before and after to ActionDetails type

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,31 @@
+type Context = {
+  outent?: any
+  inent?: any
+  query?: any
+  request?: any
+  response?: any
+}
+
+type Target = keyof Context
+
+type Set = {
+  [source in keyof Context]?: string
+}
+
+type Task = {
+  on: Target
+  field: string
+} | {
+  set: Set
+}
+
 type ActionType = "load" | "save"
 
 type ActionDetails = {
   cb_name: string
   body_args?: string[]
+  before?: Task[]
+  after?: Task[]
 }
 
 type ActionData = {
@@ -50,4 +73,4 @@ type GithubRestEndpoints =
   | "teams"
   | "users"
 
-export type { ActionType, ActionDetails, EntityMap, Entity, ActionData, SdkParams }
+export type { ActionType, ActionDetails, EntityMap, Entity, ActionData, SdkParams, Task }
